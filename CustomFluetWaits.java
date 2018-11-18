59318d97e686dc021e71cc0d6ec694c3dda2a780
Salvatore Palazzo <salvatop78@gmail.com>
2018-11-18 16:10:58 +0100

    Create CustomFluetWaits.java

---
+++ CustomFluetWaits.java
@@ -0,0 +1,19 @@
+   public class CustomFluetWaits() {
+   
+   protected WebElement waitForElementToBeClickable(int timeoutInSeconds, By by) {
+        return wait.withTimeout(timeoutInSeconds, SECONDS).until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    protected WebElement waitForElementToBeClickable(By by) {
+        return wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+    
+    
+    public boolean waitUntilElementAppears(int timeoutInSeconds, By... locators) {
+        try {
+            return wait.withTimeout(timeoutInSeconds, SECONDS).until(presentAndVisible(locators));
+        } catch (TimeoutException te) {
+            return false;
+        }
+      } 
+}
